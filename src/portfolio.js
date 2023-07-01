4e20cbd5d9cc45ae39dd090fd0999d3ce5b8a6d2
sakthiish12 <portfoliosakthiish@gmail.com>
2023-07-01 23:38:29 +0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,13 +1,13 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://github.com/sakthiish12/Portfolio',
   title: 'JS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Sakthiish',
+  role: 'Full Stack Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
